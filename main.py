e2b0925248bc66cabf9f0cc3b56636825e31d0aa
Sargis <sargis.petrosyan.arm@gmail.com>
2025-09-02 22:48:34 +0200

    .html file VIP

--- main.py
+++ main.py
@@ -1,42 +1,21 @@
-from fastapi import FastAPI
-
-from services import (
-    get_word_definitions,
-    get_word_examples,
-    get_word_synonyms,
-    get_word_antonyms,
-    get_word_usage_nots,
-    get_word_related_words,
-)
-
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from services import get_word
 
 app = FastAPI()
 
-@app.get('/{word}/definitions')
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get('/home/', response_class=HTMLResponse)
+async def redirect_home_v2(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
+
+
+@app.get('/api/words/en/{word}/',)
 async def get_definitions(word:str):
-    return await get_word_definitions(word=word)
-
-@app.get('/{word}/examples')
-async def get_examples(word:str):
-    return await get_word_examples(word=word)
-
-@app.get('/{word}/synonyms')
-async def get_synonym(word:str):
-    return await get_word_synonyms(word=word)
-
-@app.get('/{word}/antonyms')
-async def get_antonym(word:str):
-    return await get_word_antonyms(word=word)
-
-@app.get('/{word}/collections')
-async def get_collection(word:str):
-    return await get_collection(word=word)
-
-@app.get('/{word}/usage_nots')
-async def get_usage_nots(word:str):
-    return await get_word_usage_nots(word=word)
-
-@app.get('/{word}/related_words')
-async def get_related_words(word:str):
-    return await get_word_related_words(word=word)
+    return await get_word(word=word)
 
--- services.py
+++ services.py
@@ -1,17 +1,2 @@
-async def get_word_examples(word:str):
-    return word + "examples"
-
-async def get_word_definitions(word:str):
-    return word + "definitions"
-
-async def get_word_synonyms(word:str):
-    return word + "synonyms"
-
-async def get_word_antonyms(word:str):
-    return word + " antonyms"
-
-async def get_word_usage_nots(word:str):
-    return word + "usage_nots"
-
-async def get_word_related_words(word:str):
-    return word + "related_words"
+async def get_word(word:str):
+    return "word"
